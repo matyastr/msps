4ec17e7337bc20e4a5cc8e48b18c80d6dd610761
matyastr <matyastr@gmail.com>
2016-07-01 12:14:13 -0400

    Adding another test task

--- MSPS/MSPS/views.py
+++ MSPS/MSPS/views.py
@@ -19,6 +19,12 @@ tasks = [
         'title': u'Learn Python',
         'description': u'Need to find a good Python tutorial on the web', 
         'done': False
+    },
+	{
+        'id': 3,
+        'title': u'Adding a test',
+        'description': u'This is a test', 
+        'done': False
     }
 ]
 
