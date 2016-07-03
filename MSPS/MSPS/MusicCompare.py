7418f1cf9381aaa8f700c633a1b05839428023a1
matyastr <matyastr@gmail.com>
2016-07-03 19:35:40 -0400

    MSPS #6
    Had to reinstall music21

--- MSPS/MSPS/MusicCompare.py
+++ MSPS/MSPS/MusicCompare.py
@@ -2,10 +2,10 @@ from music21 import *
 
 class MusicCompare(object):
 	"""Class responsible for initiating all compare operations."""
-	def __init__(self):
-		#MusicCompare.facade = CompareFacade()
-		pass
-	def Compare(self, musicXmlFile, compareTechnique):
-		musicXmlConverter = converter.Converter()
-		inputMusicXmlFile = musicXmlConverter.parseData(musicXmlFile)
+def __init__(self):
+	#MusicCompare.facade = CompareFacade()
+	pass
+def Compare(self, musicXmlFile, compareTechnique):
+	musicXmlConverter = converter.Converter()
+	inputMusicXmlFile = musicXmlConverter.parseData(musicXmlFile)
 
